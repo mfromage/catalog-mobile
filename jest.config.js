0a46b6e7e06b20827f135d0e73befb4c6cd3f0f2
Michael Michael <hallo@michaelfromage.com>
2024-11-14 10:50:17 +0100

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  preset: 'react-native',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    './jest.setup.js',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|expo-status-bar)',
+  ],
+};
