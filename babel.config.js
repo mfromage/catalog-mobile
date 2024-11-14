0693517d87423bc033835e328c2323b7073ffdd8
Michael Michael <hallo@michaelfromage.com>
2024-11-14 10:26:41 +0100

    Add babel resolver for absolute path import, move app.tsx into src

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [
+            ".js",
+            ".jsx",
+            ".ts",
+            ".cjs",
+            ".tsx",
+            ".android.js",
+            ".android.tsx",
+            ".ios.js",
+            ".ios.tsx",
+          ],
+          root: ["."],
+          alias: {
+            "@": "./src/",
+          },
+        },
+      ],
+    ],
   };
 };
