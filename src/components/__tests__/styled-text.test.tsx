ae4e1d8ff5b5bfce10b74cb875ecc4f77666efd9
Michael Michael <hallo@michaelfromage.com>
2024-11-16 17:57:17 +0100

    Add StyledText and text-util

---
+++ src/components/__tests__/styled-text.test.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import StyledText from '../styled-text';
+import { mockPalette, render } from '@/testing/test-util';
+import { normalizeFontSize } from '@/utils/font-size';
+
+describe('StyledText', () => {
+  const setup = (props = {}) => {
+    const text = 'test text';
+    const utils = render(<StyledText {...props}>{text}</StyledText>);
+    const element = utils.getByText(text);
+    return { ...utils, element };
+  };
+  it('renders correctly with default props', () => {
+    const { element } = setup();
+    expect(element).toBeTruthy();
+  });
+
+  it('applies the correct variant style', () => {
+    const { element } = setup({ variant: 'h1' });
+    expect(element.props.style).toContainEqual(
+      expect.objectContaining({ fontSize: normalizeFontSize(34) }),
+    );
+  });
+
+  it('applies the correct color', () => {
+    const { element } = setup({ color: 'primary' });
+    expect(element.props.style).toContainEqual(
+      expect.objectContaining({ color: mockPalette.primary.main }),
+    );
+  });
+
+  it('applies semiBold style', () => {
+    const { element } = setup({ semiBold: true });
+    expect(element.props.style).toContainEqual(
+      expect.objectContaining({ fontWeight: '600' }),
+    );
+  });
+
+  it('applies italic style', () => {
+    const { element } = setup({ italic: true });
+    expect(element.props.style).toContainEqual(
+      expect.objectContaining({ fontStyle: 'italic' }),
+    );
+  });
+
+  it('applies underline style', () => {
+    const { element } = setup({ underline: true });
+    expect(element.props.style).toContainEqual(
+      expect.objectContaining({ textDecorationLine: 'underline' }),
+    );
+  });
+
+  it('applies text alignment', () => {
+    const { element } = setup({ alignment: 'center' });
+    expect(element.props.style).toContainEqual(
+      expect.objectContaining({ textAlign: 'center' }),
+    );
+  });
+});
---
+++ src/components/styled-text.tsx
@@ -0,0 +1,66 @@
+import { Platform, StyleSheet, Text, TextProps } from 'react-native';
+import { useColorPalette } from '@/providers/color-palette-provider';
+import {
+  typographyStyles,
+  TypographyVariant,
+  fontFamilyItalic,
+  TextAligment,
+  BaseColorKey,
+} from '@/themes';
+
+export type StyledTextProps = {
+  variant?: TypographyVariant;
+  color?: BaseColorKey;
+  semiBold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
+  alignment?: TextAligment;
+} & TextProps;
+
+const StyledText = ({
+  style,
+  children,
+  variant = 'body1',
+  color = 'black',
+  semiBold = false,
+  italic = false,
+  underline = false,
+  alignment = 'left',
+  ...rest
+}: StyledTextProps) => {
+  const { palette } = useColorPalette();
+  const combinedStyles = StyleSheet.flatten([
+    typographyStyles[variant],
+    { color: palette[color].main },
+  ]);
+
+  return (
+    <Text
+      style={[
+        combinedStyles,
+        semiBold && styles.semiBold,
+        italic && styles.italic,
+        underline && styles.underline,
+        { textAlign: alignment },
+        style,
+      ]}
+      {...rest}>
+      {children}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  semiBold: {
+    fontWeight: '600',
+  },
+  italic: {
+    fontStyle: 'italic',
+    fontFamily: fontFamilyItalic,
+  },
+  underline: {
+    textDecorationLine: 'underline',
+  },
+});
+
+export default StyledText;
