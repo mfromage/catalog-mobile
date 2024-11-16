fbfc70f302b8324d6ffaf6168f09f818dd2b5f4b
Michael Michael <hallo@michaelfromage.com>
2024-11-16 17:56:56 +0100

    Add theme color, dimensions and font

---
+++ src/themes/dimension.ts
@@ -0,0 +1,20 @@
+export const dimensions = {
+  spacing: {
+    xxs: 4,
+    xs: 8,
+    sm: 10,
+    md: 12,
+    lg: 16,
+    xl: 32,
+    xxl: 64,
+  },
+  borderRadius: {
+    xxs: 4,
+    xs: 8,
+    sm: 10,
+    md: 12,
+    lg: 16,
+    xl: 20,
+    xxl: 24,
+  },
+};
