02432ae99594a18c0e2034a5bcc5db071fe78886
Michael Michael <hallo@michaelfromage.com>
2024-11-17 19:29:23 +0100

    Add ScreenView

--- src/features/auth/screens/signin-screen.tsx
+++ src/features/auth/screens/signin-screen.tsx
@@ -1,22 +1,18 @@
-import { View } from 'react-native';
 import { AuthScreenNames, AuthScreenProps } from '@/types/auth-navigator.types';
 import SignInFormContainer from '../components/signin-form-container';
-import commonStyles from '@/themes/common.styles';
-import { useColorPalette } from '@/providers/color-palette-provider';
+import ScreenView from '@/components/screen-view';
 
 const SignInScreen = ({ navigation }: AuthScreenProps) => {
-  const { palette } = useColorPalette();
   const navigateToSignUp = () => navigation?.navigate(AuthScreenNames.signUp);
   const navigateToForgotPassword = () =>
     navigation?.navigate(AuthScreenNames.forgotPassword);
   return (
-    <View
-      style={[commonStyles.screen, { backgroundColor: palette.background }]}>
+    <ScreenView>
       <SignInFormContainer
         onForgotPasswordPress={navigateToForgotPassword}
         onSignUpPress={navigateToSignUp}
       />
-    </View>
+    </ScreenView>
   );
 };
 
